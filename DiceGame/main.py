c3a4595a6ccfb79ee46fcc772207e6d34a6b0a94
Developer001 <vsing8298@gmail.com>
2023-09-08 07:19:07 +0530

    Added DiceGame Class Functionality which will Enable our game for full processing

--- DiceGame/main.py
+++ DiceGame/main.py
@@ -42,18 +42,82 @@ class Player:
     def roll_dice(self):
         return self._die.roll()
     
+class DiceGame:
+    def __init__(self, player, computer):
+        self._player = player
+        self._computer = computer
 
-die = Die()
-print(die.value)
-player = Player(die, is_computer=True)
-print(f"""{player.die.value}
-{player.counter}
-{player.is_computer}""")
-new_value = player.roll_dice()
-print(new_value)
-player.increment_counter()
-print(player.counter)
-player.decrement_counter()
-player.decrement_counter()
-player.decrement_counter()
-print(player.counter)
+    def welcome_round(self):
+        print("--------------New Round Starts------------------------")
+        input("Press any key to roll the dice")
+    
+    def show_dice(self,player_value,computer_value):
+        print(f"Player1 : {player_value}")
+        print(f"Player2(Computer) : {computer_value}")
+
+    def update_counter(self, winner, loser):
+        winner.decrement_counter()
+        loser.increment_counter()
+
+    def show_counter(self):
+         print(f"Player1 Counter : {self._player.counter}\nPlayer2(Computer) Counter : {self._computer.counter}")
+    
+    def play_round(self):
+        self.welcome_round()
+        player_value = self._player.roll_dice()
+        computer_value = self._computer.roll_dice()
+        #Show the Values
+        self.show_dice(player_value, computer_value)
+        #Determine the Winner or Looser
+        if player_value > computer_value:
+            print("Player1 Wins the Round")
+            self.update_counter(winner=self._player, loser=self._computer)
+        elif computer_value > player_value:
+            print("Player2(Computer Wins the round)")
+            self.update_counter(winner=self._computer, loser=self._player)
+        else:
+            print("It's a tie")
+        
+        self.show_counter()
+
+    def show_game_over(self, winner):
+        output_str = """=====================\nGAME OVER\n====================
+                        """
+        if winner.is_computer:
+            output_str = output_str + "\nSorry The Player2(Computer) Wins the Game\nRetry Again"
+        else:
+            output_str = output_str + "\nCongratulations For your Win !\nYou Won this Game Player1"
+        output_str = output_str + "\n======================================="
+        print(output_str)
+
+    def check_game_over(self):
+        if self._player.counter == 0:
+            self.show_game_over(self._player)
+            return True
+        elif self._computer.counter == 0:
+            self.show_game_over(self._computer)
+            return True
+        else:
+            return False
+
+    def play(self):
+        output_string = """=======================================================
+                                Welcome to Roll the Dice Game
+                            ====================================================="""
+        print(output_string)
+        while True:
+            self.play_round()
+            game_over = self.check_game_over()
+            if game_over:
+                break
+    
+    
+
+player_die = Die()
+computer_die = Die()
+player1 = Player(player_die, is_computer=False)
+player2 = Player(computer_die, is_computer=True)
+game = DiceGame(player1, player2)
+
+#Start The Game
+game.play()
